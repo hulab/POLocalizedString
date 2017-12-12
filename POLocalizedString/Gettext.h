83da391f9944a437805744bb7f7ddbef51708258
Maxime Epain <maxime.epain@gmail.com>
2017-12-12 16:02:09 +0100

    Release 0.1.0

--- POLocalizedString/Gettext.h
+++ POLocalizedString/Gettext.h
@@ -26,24 +26,72 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+/**
+ Gettext representation allowing to lookup for strings in the catalog.
+ */
 @interface Gettext : NSObject
 
+/**
+ Gettext catalog headers values.
+ */
 @property (nonatomic, readonly, copy) NSDictionary<NSString *, NSString *> *headers;
 
+/**
+ Gettext catalog entries.
+ */
 @property (nonatomic, readonly, copy) NSDictionary<NSString *, POEntry *> *entries;
 
+/**
+ Number of plurals form.
+ */
 @property (readonly) NSUInteger numPlurals;
 
+/**
+ The plurals rule.
+ */
 @property (nonatomic, nullable, readonly) NSString *pluralRule;
 
+/**
+ Adds an entry to the catalog.
+
+ @param entry The entry to add.
+ */
 - (void)addEntry:(POEntry *)entry;
 
+/**
+ Sets a header value.
+
+ @param header The header key.
+ @param value The headet value.
+ */
 - (void)setHeader:(NSString *)header value:(NSString *)value;
 
+/**
+ Selects a plural form for the given count value.
+
+ @param count The count value to evaluate.
+ @return The plural form corresponding to the count.
+ */
 - (NSUInteger)selectPluralForm:(NSInteger)count;
 
+/**
+ Gets a string with the given msgid and context.
+
+ @param msgid The msgid to look up.
+ @param context The entry context.
+ @return The string value if avaialble, the msgid otherwise.
+ */
 - (NSString *)stringWithMsgid:(NSString *)msgid context:(nullable NSString *)context;
 
+/**
+ Gets a plural format with the given msgid and context.
+
+ @param msgid The msgid to look up.
+ @param msgid_plural The plural value.
+ @param count The count value to evaluate.
+ @param context The entry context.
+ @return The format value if avaialble, the msgid or msgid_plural otherwise.
+ */
 - (NSString *)stringWithMsgid:(NSString *)msgid plural:(NSString *)msgid_plural count:(NSInteger)count context:(nullable NSString *)context;
 
 @end
--- Tests/POLocalizedStringTests.m
+++ Tests/POLocalizedStringTests.m
@@ -31,28 +31,31 @@
 
 - (void)testMsgid {
     NSString *str = POLocalizedString(@"The URL is not valid and cannot be loaded.");
-    
     XCTAssertEqualObjects(str, @"Die URL is ongeldig en kan nie gelaai word nie.");
 }
 
 - (void)testAsciiFormat {
     NSString *format = POLocalizedString(@"Firefox can't find the server at %s.");
     NSString *str = [NSString stringWithFormat:format, @"location".ascii];
-    
     XCTAssertEqualObjects(str, @"Firefox kan nie die bediener vind by location nie.");
 }
 
 - (void)testUnicodeFormat {
     NSString *format = POLocalizedString(@"Firefox can't find the file at %S.");
     NSString *str = [NSString stringWithFormat:format, @"location".unicode];
-    
     XCTAssertEqualObjects(str, @"Firefox kan nie 'n lêer vind by location nie.");
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
+- (void)testPlurals {
+    NSString *format = POLocalizedString(@"Firefox can't find the file at %S.");
+    NSString *str = [NSString stringWithFormat:format, @"location".unicode];
+    XCTAssertEqualObjects(str, @"Firefox kan nie 'n lêer vind by location nie.");
+}
+
+- (void)testLoadPerformance {
     [self measureBlock:^{
-        // Put the code you want to measure the time of here.
+        self.bundle.language = @"af";
+        POLocalizedString(@"The URL is not valid and cannot be loaded.");
     }];
 }
 
