c5e493f0ce1a72d8f987fe61ab3318ea31e5183c
Maxime Epain <maxime.epain@gmail.com>
2018-04-09 19:08:36 +0200

    Fix wrapped PO files parsing

--- POLocalizedString/POParser.m
+++ POLocalizedString/POParser.m
@@ -38,139 +38,136 @@
 @implementation POParser
 
 + (nullable Gettext *)loadFile:(NSString *)path {
-	
-	NSError *error = nil;
-	NSString *content = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:&error];
+
+    NSError *error = nil;
+    NSString *content = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:&error];
     if (!content) {
         return nil;
     }
 
     Gettext *gettext = [[Gettext alloc] init];
-	NSArray *paragraphs = [content componentsSeparatedByString:@"\n\n"];
-    
+    NSArray *paragraphs = [content componentsSeparatedByString:@"\n\n"];
+
     NSString *header = paragraphs.firstObject;
-    
+
     NSArray *lines = [header componentsSeparatedByCharactersInSet:[NSCharacterSet newlineCharacterSet]];
     NSString *key = nil;
     NSString *value = nil;
-    
+
     for (NSString *line in lines) {
-        
+
         if(!line.length) {
             continue;
         }
-        
+
         // parse header
         if([line characterAtIndex:0] == '"' && [line characterAtIndex:line.length - 1] == '"') {
-            
+
             NSString *comment = [line substringWithRange:NSMakeRange(1, line.length - 2)];
             NSArray<NSString *> *arr = [comment splitByString:@":"];
-            
+
             NSString *str = [arr.firstObject.decodePO stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
-            
+
             if(arr.count < 2) {
                 value = [value stringByAppendingString:str];
                 continue;
             }
-            
+
             if (key) {
                 [gettext setHeader:value forKey:key];
             }
-    
+
             key = str;
             value = [arr[1].decodePO stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
         }
     }
-    
+
     if (key) {
         [gettext setHeader:value forKey:key];
     }
-    
+
     paragraphs = [paragraphs subarrayWithRange:NSMakeRange(1, paragraphs.count - 1)];
 
-	for (NSString *paragraph in paragraphs) {
+    for (NSString *paragraph in paragraphs) {
         POEntry *entry = [self entryFromParagraph:paragraph];
         [gettext addEntry:entry];
-	}
+    }
 
-	return gettext;
+    return gettext;
 }
 
 + (POEntry *)entryFromParagraph:(NSString *)paragraph {
-    
+
     NSArray *lines = [paragraph componentsSeparatedByCharactersInSet:[NSCharacterSet newlineCharacterSet]];
-    
+
     POEntry *entry = [POEntry new];
-    NSString *key = nil;
-    NSString *value = nil;
-    
+    NSMutableString *value = nil;
+
     for (NSString *line in lines) {
-        
+
         // parse actual entry
         NSArray<NSString *> *arr = [line splitByString:@" "];
-        
+
         if (line.length && [line characterAtIndex:0] == '#') { // #. section
             // don't use "key" here because of #_ (space) format
             // for translator comments
-            
+
             if (arr.count < 2)
                 continue;
-                
+
             unichar c = [line characterAtIndex:1];
-            value = arr[1];
-            
+
             switch(c)  {
                     // reference
                 case ':':
-                    [entry.references addObject:value];
+                    [entry.references addObject:arr[1]];
                     break;
-                    
+
                     // flag
                 case ',':
-                    [entry.flags addObject:value];
+                    [entry.flags addObject:arr[1]];
                     break;
-                    
+
                     // translator comments
                 case ' ':
-                    entry.translator_comments = value;
+                    entry.translator_comments = arr[1];
                     break;
-                    
+
                     // extracted comments
                 case '.':
-                    entry.extracted_comments = value;
+                    entry.extracted_comments = arr[1];
                     break;
-                    
+
                     // previous message, not implemented
                 case '|':
                     break;
             }
-            
+
             continue;
         }
-        
-        if (arr.count < 2) {
-            value = [value stringByAppendingString:line.decodePO.stringByRemovingQuotes];
-            continue;
-        }
-        
-        if ([key hasPrefix:@"msgstr"]) { // msgstr
+
+        if ([line hasPrefix:@"msgstr"]) { // msgstr
+            value = arr[1].decodePO.stringByRemovingQuotes.mutableCopy;
             [entry.translations addObject:value];
-        } else if([key isEqualToString:@"msgid_plural"]) { // msgid_plural
+
+        } else if ([line hasPrefix:@"msgid_plural"]) { // msgid_plural
+            value = arr[1].decodePO.stringByRemovingQuotes.mutableCopy;
             entry.msgid_plural = value;
-        } else if([key isEqualToString:@"msgid"]) { // msgid
+
+        } else if ([line hasPrefix:@"msgid"]) { // msgid
+            value = arr[1].decodePO.stringByRemovingQuotes.mutableCopy;
             entry.msgid = value;
-        } else if([key isEqualToString:@"msgctxt"]) { // msgctxt
+
+        } else if ([line hasPrefix:@"msgctxt"]) { // msgctxt
+            value = arr[1].decodePO.stringByRemovingQuotes.mutableCopy;
             entry.context = value;
+
+        } else {
+            [value appendString:line.decodePO.stringByRemovingQuotes];
+            continue;
         }
-        
-        key = arr.firstObject;
-        value = arr[1].decodePO.stringByRemovingQuotes;
     }
-    
-    if ([key hasPrefix:@"msgstr"]) { // msgstr
-        [entry.translations addObject:value];
-    }
-    
+
     return entry;
 }
 
@@ -180,23 +177,23 @@
 
 - (NSString *)decodePO {
     NSString *string = self.copy;
-    
+
     string = [string stringByReplacingOccurrencesOfString:@"\\\\" withString:@"\\"];
     string = [string stringByReplacingOccurrencesOfString:@"\\t" withString:@"\t"];
     string = [string stringByReplacingOccurrencesOfString:@"\\n" withString:@"\n"];
     string = [string stringByReplacingOccurrencesOfString:@"\\\"" withString:@"\""];
-    
+
     return string;
 }
 
 - (NSString *)encodePO {
     NSString *string = self.copy;
-    
+
     string = [string stringByReplacingOccurrencesOfString:@"\\" withString:@"\\\\"];
     string = [string stringByReplacingOccurrencesOfString:@"\\t" withString:@"\t"];
     string = [string stringByReplacingOccurrencesOfString:@"\n" withString:@"\\n"];
     string = [string stringByReplacingOccurrencesOfString:@"\"" withString:@"\\\""];
-    
+
     return string;
 }
 
@@ -204,39 +201,39 @@
     NSMutableArray *array = [[NSMutableArray alloc] init];
     NSScanner *scan = [NSScanner scannerWithString:self];
     NSString *token;
-    
+
     if([scan scanUpToString:separator intoString:&token]) {
         NSUInteger pos = scan.scanLocation + 1;
         [array addObject:token];
-        
+
         if(pos < self.length) {
             [array addObject:[self substringFromIndex:pos]];
         }
     }
-    
+
     return [NSArray arrayWithArray:array];
 }
 
 - (NSString *)stringByRemovingQuotes {
     NSString *string = self.copy;
-    
+
     NSUInteger x = 0, y = 0;
-    
+
     y = string.length;
-    
+
     if (y) {
-        
+
         if([string characterAtIndex:0] == '"') {
             x++;
         }
-        
+
         if(x < y && [string characterAtIndex:string.length - 1] == '"') {
             y--;
         }
-        
+
         string = [string substringWithRange:NSMakeRange(x, y - x)];
     }
-    
+
     return string;
 }
 
